96f28abea0f4e2c27c077fd8fad91ea28a79d98b
PhongDoan69 <87539498+PhongDoan69@users.noreply.github.com>
2023-04-20 13:35:07 +0700

    Update DatSachTest.java

--- QuanLyThuVien/src/test/java/Test/DatSachTest.java
+++ QuanLyThuVien/src/test/java/Test/DatSachTest.java
@@ -27,23 +27,6 @@ public class DatSachTest {
     @Test
     //Test đặt sách
     public void testDatSach() throws SQLException{
-//        datTiecSV = new DatTiecServices();
-//        hoaDonSV = new HoaDonServices();
-//        DatTiec d = new DatTiec();
-//        d.setMaTiec(maTiec);
-//        d.setMaKH(2);
-//        d.setNgayToChuc(java.sql.Date.valueOf(LocalDate.now()));
-//        d.setNgayDat(java.sql.Date.valueOf(LocalDate.now()));
-//        d.setSoLuongBan(10);
-//        d.setSoLuongKhach(100);
-//        d.setTenTiec("Tiệc cưới");
-//        d.setMaSanh(1);
-//        d.setBuoi("Sáng");
-//        int a = datTiecSV.getListDatTiec(null).size();
-//        datTiecSV.addDatTiec(d);
-//        HoaDon h = new HoaDon();
-//        h.setMaTiec(maTiec);
-//        hoaDonSV.addHoaDon(h);
-//        Assertions.assertEquals(a + 1, datTiecSV.getListDatTiec(null).size());
+//    
     }
 }
